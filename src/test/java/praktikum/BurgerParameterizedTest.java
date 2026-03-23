efb0ada27b499a8ba84da5168f220739e55ca30d
Anastasia <an.rad.se@gmail.com>
2026-03-23 21:47:48 +0300

    Добавила 2 теста

---
+++ src/test/java/praktikum/BurgerParameterizedTest.java
@@ -0,0 +1,62 @@
+package praktikum;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class BurgerParameterizedTest {
+    private Burger burger;
+    @Mock
+    private Bun bun;
+
+    @Mock
+    private Ingredient ingredient;
+
+    private final float bunPrice;
+    private final float ingredientPrice;
+    private final float expectedPrice;
+
+    public BurgerParameterizedTest(float bunPrice, float ingredientPrice, float expectedPrice) {
+        this.bunPrice = bunPrice;
+        this.ingredientPrice = ingredientPrice;
+        this.expectedPrice = expectedPrice;
+
+    }
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        burger = new Burger();
+    }
+    @Parameterized.Parameters(name = "Цена бургера: булка={0}, ингредиент={1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                // нормальные случаи
+                {300f, 100f, 700f},
+                {100f, 200f, 400f},
+                {200f, 0f, 400f},
+                // граничный случай, нулевые цены
+                {0f, 0f, 0f},
+                // крайний случай, очень дорогой ингридиент
+                {100f, 1000f, 1200f},
+
+        });
+    }  @Test
+    public void checkBurgerPriceCalculation() {
+        Mockito.when(bun.getPrice()).thenReturn(bunPrice);
+        Mockito.when(ingredient.getPrice()).thenReturn(ingredientPrice);
+
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+
+        Assert.assertEquals(expectedPrice, burger.getPrice(), 0.001f);
+    }
+}
+
